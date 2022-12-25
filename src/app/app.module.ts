62c3881d7fea8001dfe7cf369ee6feffe53fc3bd
Unknown <unknown@example.com>
2022-12-25 16:49:55 +0330

    add a input and update this component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchBarComponent } from './search-bar/search-bar.component';
 import { PageListComponent } from './page-list/page-list.component';
-import { TestComponent } from './test/test.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SearchBarComponent,
-    PageListComponent,
-    TestComponent
+    PageListComponent
+    
   ],
   imports: [
     BrowserModule,
--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
   styleUrls: ['./search-bar.component.css']
 })
-export class SearchBarComponent {
+export class SearchBarComponent implements OnInit {
+  term: string = ''
 
+  ngOnInit(): void {}
+
+  onSubmit(event: any) {
+    console.log(this.term)
+    event.preventDefault()
+  }
 }
