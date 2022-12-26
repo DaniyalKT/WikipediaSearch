d7d8a2903bb9e08051c1075754c7f6be59197dc9
Unknown <unknown@example.com>
2022-12-26 15:32:07 +0330

    send response to pageList component

--- src/app/page-list/page-list.component.ts
+++ src/app/page-list/page-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-page-list',
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./page-list.component.css']
 })
 export class PageListComponent {
+@Input() searchResponse: [] = []
 
+constructor(){
+  console.log(this.searchResponse)
+}
 }
